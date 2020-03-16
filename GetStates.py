d7882c7ba6c679ae7b0bf86392a2505359592879
Connor Nelson <55152327+Connor205@users.noreply.github.com>
2020-03-16 19:16:42 -0400

    Update GetStates.py
    Adding Comments and TQDM

--- GetStates.py
+++ GetStates.py
@@ -1,16 +1,19 @@
-from urllib.request import urlopen 
+from urllib.request import urlopen
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 import sys
-import re
 
 __author__ = "Connor Nelson"
 
-#Opening and Reading in HTML
+# Opening and Reading in HTML File
 html = urlopen("https://abbreviations.yourdictionary.com/articles/state-abbrev.html")
 bsObj = BeautifulSoup(html, "html.parser")
+# Get the list objects
 linkedList = bsObj.findAll('li')
 goodList = []
-for item in linkedList:
-	goodList.append(item.text)
-print(goodList[:50])
+# Take first 50 and put them in a list
+for i in tqdm(range(50)):
+	goodList.append(linkedList[i].text)
+ # Print List
+print(goodList)
 
