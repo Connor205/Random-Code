de8984cc1263f461a64a89b137c6ddfbbf8814ce
Connor Nelson <conn205@gmail.com>
2020-10-19 15:56:27 -0400

    renaming files

--- Testing.py
+++ Testing.py
@@ -1,11 +1,11 @@
 def printFib():
-	FibList = []
-	FibList.append(0)
-	FibList.append(1)
-	for i in range(100 - 2):
-		FibList.append(FibList[i] + FibList[i + 1])
-	print(FibList)
-	# print(len(FibList))
+  FibList = []
+  FibList.append(0)
+  FibList.append(1)
+  for i in range(100 - 2):
+    FibList.append(FibList[i] + FibList[i + 1])
+    print(FibList)
+# print(len(FibList))
 
 if __name__ == '__main__':
   printFib()
