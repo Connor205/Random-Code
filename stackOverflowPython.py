3ea317502bd1fb489ad2b56b77e3d8d89f9b3509
connor205 <Conn205@gmail.com>
2020-11-04 12:48:15 -0500

    Adding stackOverflow answers testing

---
+++ stackOverflowPython.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+array = [["X", "O", ""],["X", "O", ""],["X", "O", ""]]
+print(np.matrix(array))
+
+# There are two ways I would recommend doing this. 
+# The first way is using numpy. 
+
+#     input numpy as np
+#     array = [["X", "O", ""],["X", "O", ""],["X", "O", ""]]
+#     print(np.matrix(array))
+
+# The second way would be to iterate through the entire board and print it
+
+#     array = [["X", "O", ""],["X", "O", ""],["X", "O", ""]]
+#     for row in array:
+#       for val in row:
+#         print(val, end="")
+#       print() # This line adds a newline 
+    
+
+
+
+
+
