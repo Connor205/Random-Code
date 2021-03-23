3b5a621f3c0f8a9808dda539052f43a5460a09ff
connor205 <Conn205@gmail.com>
2021-03-23 09:53:40 -0400

    testing tabnine

--- Testing.py
+++ Testing.py
@@ -1,11 +1,13 @@
-def printFib():
-  FibList = []
-  FibList.append(0)
-  FibList.append(1)
-  for i in range(100 - 2):
-    FibList.append(FibList[i] + FibList[i + 1])
-    print(FibList)
-# print(len(FibList))
+def print_fib():
+    """This function prints the fib sequence for 100 digits.
+    """
+    fib_list = []
+    fib_list.append(0)
+    fib_list.append(1)
+    for i in range(100 - 2):
+        fib_list.append(fib_list[i] + fib_list[i + 1])
+        print(fib_list)
+
 
 if __name__ == '__main__':
-  printFib()
+    print_fib()
